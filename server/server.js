5d4c61e39da36e477e71c32e01ac5bca3b1dbdef
Ayako Woollan <ayako.woollan@gmail.com>
2022-04-10 21:21:03 +1000

    updating port

--- server/server.js
+++ server/server.js
@@ -9,4 +9,4 @@ app.use(express.json());
 
 require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () => console.log(`Now listening on port: http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Now listening on http://localhost:${PORT}`));
